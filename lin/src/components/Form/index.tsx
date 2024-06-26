02b01275befb0b69fe8959f2ad577585e5ff329d
lilinji-TT <112566451+lilinji-TT@users.noreply.github.com>
2024-06-27 00:19:07 +0800

    feat：水印组件

--- lin/src/components/Form/index.tsx
+++ lin/src/components/Form/index.tsx
@@ -1,3 +1,10 @@
+/**
+ * Form 组件原理大体就是将表单项，
+ * 使用context保存，
+ * 然后在item中拿出来改变，
+ * 进行验证这些操作，
+ * 修改保存在context中的值
+ */
 import { From as InternalForm } from "./Form";
 import { Item } from "./Item";
 type InternalFormType = typeof InternalForm;
